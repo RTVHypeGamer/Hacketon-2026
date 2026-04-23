2703de8dcaa7d5d5708d28f5b0ef8717758ed876
fsdev <dev@foursquared.dev>
2026-04-23 17:23:56 +0200

    adv movement 1

--- Assets/Main/Scripts/PlayerMovement.cs
+++ Assets/Main/Scripts/PlayerMovement.cs
@@ -4,27 +4,43 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float moveSpeed = 4f;
+    public float moveSpeed = 7f;
     public float jumpForce = 5f;
     public LayerMask groundMask;
+    public float MaxEnergy = 100f;
+
+    // Sprinting
+    public KeyCode sprintKey = KeyCode.LeftShift;
+    public float sprintSpeedMultiplier = 1.5f;
+    public float sprintEnergyCost = 15f;
 
     private Rigidbody rb;
     private bool isGrounded;
+    private float currentEnergy;
+    private bool isSprinting;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         rb.freezeRotation = true;
+        currentEnergy = MaxEnergy;
     }
 
-    // Update is called once per frame
     void Update()
     {
        if(Input.GetButtonDown("Jump") && isGrounded)
         {
             rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-        }  
+        }
+
+        if(Input.GetKey(sprintKey) && currentEnergy > 0)
+        {
+            isSprinting = true;
+        }
+        else
+        {
+            isSprinting = false;
+        }
     }
 
     private void FixedUpdate()
@@ -32,10 +48,21 @@ public class PlayerMovement : MonoBehaviour
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
 
-        Vector3 move = (transform.forward * v + transform.right * h) * moveSpeed;
+        float speed = moveSpeed;
+        if(isSprinting)
+        {
+            speed *= sprintSpeedMultiplier;
+            currentEnergy -= sprintEnergyCost * Time.fixedDeltaTime;
+            currentEnergy = Mathf.Max(0, currentEnergy);
+        }
+
+        Vector3 move = (transform.forward * v + transform.right * h) * speed;
         Vector3 newVelocity = new Vector3(move.x, rb.linearVelocity.y, move.z);
         rb.linearVelocity = newVelocity;
 
         isGrounded = Physics.Raycast(transform.position, Vector3.down, 1.1f, groundMask);
     }
 }
+
+// Movement abilities ??:
+// - Wall Jump
