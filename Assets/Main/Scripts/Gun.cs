393b04e1b03c672a02079663b32bb189f3935a7b
beepboop <89303943+Discotijger@users.noreply.github.com>
2026-04-24 05:37:39 +0200

    Schooting Functional
    Shooting works but cant remove hp from enemies because they have no hp

---
+++ Assets/Main/Scripts/Gun.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public float damage = 10f;
+    public float range = 30f;
+    public Camera Maincamera;
+    public EnemyAi[] EnemyAi;
+    EnemyAi target;
+
+    // public ParticleSystem Muzzelflash;
+    // public GameObject Flareeffect;
+    public float Muzzelforce = 30f;
+    // private Enemy target;
+    //public AudioSource shootAudioSource;
+    //public soundClips SoundClips;
+    
+     private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            shoot();
+        }     
+        
+         void shoot()
+        {
+        
+           if (Physics.Raycast(Maincamera.transform.position, Maincamera.transform.forward, out RaycastHit hit, range))
+            {
+                Debug.Log(hit.transform.name);
+                target = hit.transform.GetComponent<EnemyAi>();
+                if(hit.transform.GetComponent<EnemyAi>())
+                
+                if(target != null){
+                    
+                }
+            }
+        }
+    }
+}
